991dfc9e78bba8852cb14f322356c1fa55d4d164
Andrew Locke <alocke12992@gmail.com>
2018-03-15 11:51:21 -0600

    Adds faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  serialize :lied_cats, Array
+    def self.random_cat(ids) 
+      Cat.where("id NOT IN (?)", ids).order("RANDOM()")
+    end 
 end
